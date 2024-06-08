3e7237dcdaec387e41e78914e6de45e62ea7d3af
NikitaKalchevsky <mazdozavr@gmail.com>
2024-06-08 19:33:43 +0300

    edit name url slider index

--- app/js/main.js
+++ app/js/main.js
@@ -36,31 +36,6 @@ $(
   ".header-bottom__filter-sort, .header-bottom__filter-sort, .page-catalog__filter-sort, .page-catalog__filter-show, .product-info__buy-num "
 ).styler();
 
-//slider
-// $(".top-slider__inner").slick({
-//   dots: false,
-//   infinite: true,
-//   arrows: true,
-//   speed: 500,
-//   autoplay: false,
-//   autoplaySpeed: 2000,
-//   slidesToShow: 1,
-//   slidesToScroll: 1,
-//   prevArrow:
-//     '<button type="button" class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" id="arrow"><path fill="#134563" d="M37.9 46 24.1 32.3l13.8-13.7 2 2-11.8 11.7L39.9 44l-2 2"></path></svg></button>',
-//   nextArrow:
-//     '<button type="button" class="slick-next"><svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 64 64" viewBox="0 0 64 64" id="arrow"><path fill="#134563" d="m-210.9-289-2-2 11.8-11.7-11.8-11.7 2-2 13.8 13.7-13.8 13.7" transform="translate(237 335)"></path></svg></button>',
-
-//   breakpoint: 993,
-//   settings: {
-//     draggable: true,
-//     arrows: false,
-//     dots: true,
-//     autoplay: false,
-//   },
-// });
-//--------------------------------------------------------------
-
 //MIXitUP
 document.addEventListener("DOMContentLoaded", function () {
   if (document.querySelector(".product__items")) {
@@ -122,6 +97,7 @@ $(".star").rateYo({
     '<path d="M0.0229731 6.16432C0.0780973 5.9946 0.224753 5.87091 0.401315 5.84529L5.36139 5.12451L7.57966 0.629933C7.6586 0.469933 7.82157 0.368652 7.99997 0.368652C8.17841 0.368652 8.34135 0.469933 8.42032 0.629933L10.6387 5.12451L15.5987 5.84529C15.7752 5.87091 15.9219 5.9946 15.977 6.16429C16.0322 6.334 15.9862 6.52028 15.8584 6.64481L12.2694 10.1434L13.1165 15.0834C13.1467 15.2593 13.0744 15.437 12.9301 15.5419C12.8484 15.6012 12.7517 15.6314 12.6545 15.6314C12.5799 15.6314 12.505 15.6136 12.4365 15.5776L8 13.2451L3.56374 15.5775C3.40577 15.6606 3.21443 15.6467 3.07009 15.5419C2.92574 15.437 2.8534 15.2593 2.88356 15.0834L3.73096 10.1434L0.141566 6.64478C0.0138168 6.52028 -0.0322151 6.334 0.0229731 6.16432Z"/>' +
     "</svg>",
 });
+//---------------------------
 $(".star-nofix").rateYo({
   starWidth: "17px",
   spacing: "6px",
@@ -133,6 +109,7 @@ $(".star-nofix").rateYo({
     '<path d="M0.0229731 6.16432C0.0780973 5.9946 0.224753 5.87091 0.401315 5.84529L5.36139 5.12451L7.57966 0.629933C7.6586 0.469933 7.82157 0.368652 7.99997 0.368652C8.17841 0.368652 8.34135 0.469933 8.42032 0.629933L10.6387 5.12451L15.5987 5.84529C15.7752 5.87091 15.9219 5.9946 15.977 6.16429C16.0322 6.334 15.9862 6.52028 15.8584 6.64481L12.2694 10.1434L13.1165 15.0834C13.1467 15.2593 13.0744 15.437 12.9301 15.5419C12.8484 15.6012 12.7517 15.6314 12.6545 15.6314C12.5799 15.6314 12.505 15.6136 12.4365 15.5776L8 13.2451L3.56374 15.5775C3.40577 15.6606 3.21443 15.6467 3.07009 15.5419C2.92574 15.437 2.8534 15.2593 2.88356 15.0834L3.73096 10.1434L0.141566 6.64478C0.0138168 6.52028 -0.0322151 6.334 0.0229731 6.16432Z"/>' +
     "</svg>",
 });
+//--------------------------------
 $(".product-tabs__top-item").on("click", function (e) {
   e.preventDefault();
   $(".product-tabs__top-item").removeClass("product-tabs__top-item--active");
@@ -157,7 +134,7 @@ var swiper = new Swiper(".swiper", {
     prevEl: ".swiper-button-prev",
   },
 });
-
+//-----------swiperJS--------------
 var swiper2 = new Swiper(".product-other__gallary", {
   loop: true,
   speed: 1500,
@@ -197,15 +174,4 @@ var swiper3 = new Swiper(".top-slider", {
     nextEl: ".top-slider__next",
     prevEl: ".top-slider__prev",
   },
-
-  // breakpoints: {
-  //   640: {
-  //     slidesPerView: 1,
-  //     spaceBetween: 10,
-  //   },
-  //   768: {
-  //     slidesPerView: 2,
-  //     spaceBetween: 20,
-  //   },
-  // },
 });
